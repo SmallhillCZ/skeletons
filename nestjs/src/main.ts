47f7499bd9735f4080f8f257243d7d5c5a34ea28
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-12-05 03:41:43 +0000

    feat(nestjs): add views

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 import { AppModule } from "./app.module";
 import { Config } from "./config";
 import { registerOpenAPI } from "./openapi";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
 	const config = app.get(Config);
 
-	registerOpenAPI("openapi", app, config);
+	app.useStaticAssets(join(__dirname, "..", "public"));
+	app.setBaseViewsDir(join(__dirname, "..", "views"));
+
+	registerOpenAPI("api", app, config);
 
 	await app.listen(config.server.port, config.server.host);
 }
