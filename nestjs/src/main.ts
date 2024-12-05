2a009f0bcce94c86617d85d0535fed7a5071f7f5
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-12-05 04:23:30 +0000

    feat(nestjs): add templating config

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,21 +1,27 @@
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
-import { join } from "path";
+
+import { Logger } from "@nestjs/common";
 import { AppModule } from "./app.module";
 import { Config } from "./config";
 import { registerOpenAPI } from "./openapi";
+import { registerTemplating } from "./templating";
 
 async function bootstrap() {
+	const logger = new Logger("MAIN");
+
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
 	const config = app.get(Config);
 
-	app.useStaticAssets(join(__dirname, "..", "public"));
-	app.setBaseViewsDir(join(__dirname, "..", "views"));
-	app.setViewEngine("hbs");
+	// comment to disable templating
+	registerTemplating(app);
 
+	// comment to disable OpenAPI and Swagger
 	registerOpenAPI("api", app, config);
 
 	await app.listen(config.server.port, config.server.host);
+
+	logger.log(`Server running on http://${config.server.host}:${config.server.port}`);
 }
 bootstrap();
